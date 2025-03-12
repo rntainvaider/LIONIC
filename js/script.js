6aa22db2c27ff7003cd32291f794453098c121c9
evgenii <vovk.evgenii.199811@gmail.com>
2025-03-12 16:44:32 +0300

    переписываю с самого начала

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const Burger = document.querySelector(".burger");
-
-Burger.addEventListener("click", () => {
-    const HeaderNav = document.querySelector(".header__nav");
-    const currentDisplay = window.getComputedStyle(HeaderNav).display;
-
-    HeaderNav.style.display = (currentDisplay === "none") ? "flex" : "none";
-});
